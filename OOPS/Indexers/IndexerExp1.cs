9f4e677a906473b96ddb643a2efff822e1ac04bf
UmaMaheshK <uma.kondapally@gmail.com>
2018-01-13 15:18:27 +0530

    Changed the first indexer value

--- OOPS/Indexers/IndexerExp1.cs
+++ OOPS/Indexers/IndexerExp1.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OOPS.Indexers
 {
@@ -10,7 +6,6 @@ namespace OOPS.Indexers
     {
         class Indexer1
         {
-
             private string[] dayNames = new string[] { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY" };
 
             public string this[int index]
@@ -24,27 +19,42 @@ namespace OOPS.Indexers
                     dayNames[index] = value;
                 }
             }
+
             public void AllDays()
             {
-                for (int i = 0; i < dayNames.Length; i++)
-                {
-                    Console.WriteLine("dayNames[{0}] = {1}", i, dayNames[i]);
-                }
+                //for (int i = 0; i < dayNames.Length; i++)
+                //{
+                //    Console.WriteLine("dayNames[{0}] = {1}", i, dayNames[i]);
+                //}
+                int index = default(int);
+                Array.ForEach(dayNames, dady => Console.WriteLine("dayNames[{0}] = {1}", index++, dady));
             }
         }
 
         static void Main(string[] args)
         {
             Indexer1 indexer = new Indexer1();
-            Console.WriteLine("Display all day names");
-            indexer.AllDays();            
-            //get second element
-            Console.WriteLine("day");
+            Console.WriteLine("Befor display all day names\n");
+            indexer.AllDays();
+            indexer[0] = "Uma mahesh";
+            Console.WriteLine("\nAfter changed indexer display all day names\n");
+            indexer.AllDays();
         }
 
         /*  OUTPUT
-                Display all day names
+              Befor display all day names
+
                 dayNames[0] = SUNDAY
+                dayNames[1] = MONDAY
+                dayNames[2] = TUESDAY
+                dayNames[3] = WEDNESDAY
+                dayNames[4] = THURSDAY
+                dayNames[5] = FRIDAY
+                dayNames[6] = SATURDAY
+
+                After changed indexer display all day names
+
+                dayNames[0] = Uma mahesh
                 dayNames[1] = MONDAY
                 dayNames[2] = TUESDAY
                 dayNames[3] = WEDNESDAY
